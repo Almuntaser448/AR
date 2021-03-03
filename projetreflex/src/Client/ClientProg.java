c0879d4dae59f2c5377410dc49dfc3db9323459d
Almuntaser448 <alm1b1b1@gmail.com>
2021-03-03 12:23:58 +0100

    intialitation de programeur

--- projetreflex/src/Client/ClientProg.java
+++ projetreflex/src/Client/ClientProg.java
@@ -1,5 +1,27 @@
 package Client;
 
+
 public class ClientProg {
+	private String login;
+	private String mdp;
+	private String ftp;
+
+	public ClientProg(String login, String mdp, String ftp) {
+		this.login = login;
+		this.mdp = mdp;
+		this.ftp = ftp;
+	}
+	public void setFtp(String ftp) {
+		this.ftp=ftp;
+	}
+	public String getLogin() {
+		return this.login;
+	}
+	public String getMdp() {
+		return this.mdp;
+	}
+	public String getFtp() {
+		return this.ftp;
+	}
 
 }
